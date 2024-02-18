7e3b3c68664c3c504f85aad84d4f38f09969507b
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2024-02-18 23:31:50 +0530

    Triplet Sum in Array
    T(n) = O(N^2)
    S(n) = O(1)

---
+++ GFG/tripletsumarr.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {
+        //Your Code Here
+        sort(A,A+n);
+        for(int i= 0; i<n-2; i++){
+            int key = X  - A[i] , s = i+1, e = n-1;
+            while(s<e){
+                int val = A[s] + A[e];
+                if(key == val)return true;
+                else if(key<val)e--;
+                else s++;
+            }
+        }
+        
+        return false;
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n,X;
+		cin>>n>>X;
+		int i,A[n];
+		for(i=0;i<n;i++)
+			cin>>A[i];
+		Solution ob;
+        cout <<  ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
