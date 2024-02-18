e0d7a9f0849d0f76c2c3f7fccb283d3d0fda50c9
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2024-02-19 00:19:09 +0530

    Segregate 0s and 1s
    T(n) = O(n)
    S(n)=O(1)

---
+++ GFG/segregate0and1.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    void segregate0and1(int arr[], int n) {
+        // code here
+        for(int s = 0, e = n-1 ; s <e ;)arr[s]?swap(arr[s],arr[e]),e--:s++;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.segregate0and1(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
