6741c8f90b5a1eb9a7fb426a47d75cf701832f44
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2024-02-23 12:48:37 +0530

    Create brute.cpp
    T(N) = O(N^2)
    S(N) = O(1)

---
+++ GFG/Longest Sub-Array with Sum K/brute.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int lenOfLongSubarr(int A[],  int N, int K) 
+    { 
+        // Complete the function
+        int len = 0;
+        for(int i = 0; i< N ; i++){
+            int sum = 0;
+            for(int j = i; j<N; j++ ){
+                sum += A[j];
+                if(sum == K)len = max(len,j-i+1);
+            }
+        }
+        return len;
+    } 
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;cin>>t;
+	while(t--)
+	{
+	    int n, k;
+	    cin>> n >> k;
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution ob;
+	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
