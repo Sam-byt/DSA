6a8704e94c1d291dec4e45af2603179b6eb8af52
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2024-03-11 21:31:58 +0530

    Better Solution
    Optimized with a checker
    
    T(N) = O(N^2 max)
    S(N) = O(N)

---
+++ GFG/Leaders in an Array/better.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+        vector<bool> check(n,0);
+        check[n-1]  = 1;
+        for(int i = n-2; i>=0 ; i--){
+            int key = a[i];
+            for(int j = i+1; j<n; j++){
+                if(key >= a[j]){
+                    if(check[j]){
+                        check[i] = 1;
+                        break;
+                    }
+                }
+                else break;
+            }
+        }
+        
+        
+        vector<int>res;
+        for(int i =0; i<n; i++){
+            if(check[i])res.push_back(a[i]);
+        }
+        
+        return res;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+
+// } Driver Code Ends
