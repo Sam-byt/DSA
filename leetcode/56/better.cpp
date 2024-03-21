590580e794ba0e13f78aac8bc216fba78f4c1305
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2024-03-21 22:08:30 +0530

    Merge Intervals
    Optimizing the brute force solution with normal sort and reducing few unnecesssary lines logically
    
    T(N) = O(NlogN) for sort
    S(N) = O(2*N)  for storing the solutiion only

---
+++ leetcode/56/better.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        int s = intervals[0][0], e = intervals[0][1];
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][1] <= e)
+                continue;
+            if (intervals[i][0] <= e)
+                e = max(e, intervals[i][1]);
+            else {
+                res.push_back({s, e});
+                s = intervals[i][0], e = intervals[i][1];
+            }
+        }
+
+        res.push_back({s, e});
+
+        return res;
+    }
+};
